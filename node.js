32cd7601274e8b26dd00aa0e2a28a255f1bd8d2e
juani <juaninv@outlook.com>
2016-12-05 13:10:36 -0300

    ovnda

--- node.js
+++ node.js
@@ -1,46 +1,65 @@
-var fs = require('fs');
 var http = require("http");
-var server = http.createServer();
 var url = require("url");
-var ipVisitan = {}
+var noticiasService = require('./manejadorArticulos');
+var mu = require('mu2');
+var server = http.createServer();
+var fs = require('fs');
+
+var express = require("express");
+var app = express();
 
 
-server.on("request", function(req,res){
-    var urlData=url.parse(req.url);
+
+mu.root = __dirname + '/public';
+// mu.root = __dirname;
+
+var page = {};
+
+server.on ("request", function (req, res) {
+
+    var urlData = url.parse(req.url, true);
     var path = urlData.pathname;
-    var filePath= "public"+path;
-    var ip = req.connection.remoteAddress;
 
-    ipVisitan[ip] = 0;
-    console.log(ipVisitan);
+    // if(path == '/' || path == '/noticias') {
+    if(path == '/' || path == '/index.html') {
+        //WORK WITH MUSTACHE
 
-    //res.end(ip);
-    fs.exists(filePath,function(exists){
-        if(exists){
-            fs.readFile(filePath, function(err,data){
-                if (err){
-                    res.writeHead(500);
-                    if (path=="/"){
-                        filePath = "public/index.html";
-                        fs.readFile(filePath, function(err,data){
+        mu.clearCache();
+
+        page.title = 'Reddit - Curso - Dinamico';
+        page.description = '';
+        var noticias = noticiasService.get();
+        // var stream = mu.compileAndRender('index.html', {page: page, nombre: "Jonathan", noticias: noticiasService.get()});
+        var stream = mu.compileAndRender('index.html', {title: "Reddit", noticias: noticias});
+
+        stream.pipe(res);
+    }
+    else{
+        //Trabajo como antes
+
+        var filePath= "public"+path;
+        fs.exists(filePath,function(exists) {
+            if (exists) {
+                fs.readFile(filePath, function (err, data) {
+                    if (err) {
+                        res.writeHead(500);
+                        if (path == "/") {
+                            filePath = "public/index.html";
+                            fs.readFile(filePath, function (err, data) {
                                 res.end(data);
-                        })
+                            })
+                        }
+                    } else {
+                        res.end(data);
                     }
-                }else{
-                    res.end(data);
-                }
-            })
-        }else {
-            res.writeHead(404);
-            filePath = "public/404.html";
-            fs.readFile(filePath, function(err,data){
-                res.end(data);
-            })
-
-        }
-    });
+                })
+            } else {
+                res.writeHead(404);
+                res.end("No existe");
+            }
+        })
+    }
 });
-
 
 
 server.listen(process.env.PORT || 3000);
